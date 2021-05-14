633aaa8a6152463eda5e369d0ba7d5bf0227c480
Breno Campos <brenocamposr@gmail.com>
2021-05-14 11:28:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var date1 = document.getElementById("ano1");
+var date2 = document.getElementById("ano2");
+var d = new Date();
+
+date1.innerHTML = `2014 - ${d.getFullYear()} Red Target Lab`;
+date2.innerHTML = `2014 - ${d.getFullYear()} Red Target Lab`;
